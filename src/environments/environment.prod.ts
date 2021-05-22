f3799fe031711269981431c0663c7b9c2cfc2c06
Abel Ani <abelanico6@gmail.com>
2021-05-22 14:04:01 +0100

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBxtAyJ4Y4jxpr1KbAZugYsovphi10uh98',
-    authDomain: 'origami-portal-ead60.firebaseapp.com',
-    databaseURL: 'https://origami-portal-ead60.firebaseio.com',
-    projectId: 'origami-portal-ead60',
-    storageBucket: 'origami-portal-ead60.appspot.com',
-    messagingSenderId: '354284544073',
-    appId: '1:354284544073:web:0475f021a48c00c00d5f98'
-  },
+  firebaseConfig: {},
 };
